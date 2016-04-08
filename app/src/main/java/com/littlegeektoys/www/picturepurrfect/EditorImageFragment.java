cb18b05c30a2628a5f51b5dc4872b342af7d5564
mikarman <mkarman@mymail.mines.edu>
2016-04-08 16:54:30 -0600

    update 1

--- app/src/main/java/com/littlegeektoys/www/picturepurrfect/EditorImageFragment.java
+++ app/src/main/java/com/littlegeektoys/www/picturepurrfect/EditorImageFragment.java
@@ -1,11 +1,33 @@
 package com.littlegeektoys.www.picturepurrfect;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Michael Karman on 4/8/2016.
  */
 public class EditorImageFragment extends Fragment {
     private static final String TAG = "EditorImageFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView() called");
+
+        View v = inflater.inflate(R.layout.???, container, false);
+
+        return v;
+    }
 
 }
