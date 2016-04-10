c58df02b9c1253c2d0ddd96e86c9468b589046f9
mikarman <mkarman@mymail.mines.edu>
2016-04-10 16:41:29 -0600

    working on EditorActivity definition

--- app/src/main/java/com/littlegeektoys/www/picturepurrfect/EditorActivity.java
+++ app/src/main/java/com/littlegeektoys/www/picturepurrfect/EditorActivity.java
@@ -1,7 +1,21 @@
 package com.littlegeektoys.www.picturepurrfect;
 
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Michael Karman on 4/10/2016.
  */
-public class EditorActivity {
+public class EditorActivity extends AppCompatActivity {
+
+    @LayoutRes
+    protected int getLayoutResId() { return R.layout.editor_fragment_container};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+    }
 }
