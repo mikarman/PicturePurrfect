da4c6e4cd493fb8652af360d438baa0ff0b069bc
Jesse DeMott <jdemott@mines.edu>
2016-04-22 11:24:10 -0600

    Created initial CanvasView

---
+++ app/src/main/java/com/littlegeektoys/www/picturepurrfect/CanvasView.java
@@ -0,0 +1,21 @@
+package com.littlegeektoys.www.picturepurrfect;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by Jesse DeMott on 4/22/2016.
+ */
+public class CanvasView extends View {
+
+    public CanvasView(Context context)   {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+}
