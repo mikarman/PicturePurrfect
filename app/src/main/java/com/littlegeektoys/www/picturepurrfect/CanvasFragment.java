a89e141c10cab5ec43be2ab2d636f3be64e34219
Jesse DeMott <jdemott@mines.edu>
2016-04-22 12:00:09 -0600

    Created initial CanvasFragment

---
+++ app/src/main/java/com/littlegeektoys/www/picturepurrfect/CanvasFragment.java
@@ -0,0 +1,55 @@
+package com.littlegeektoys.www.picturepurrfect;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+/**
+ * Created by Jesse DeMott on 4/22/2016.
+ */
+public class CanvasFragment extends Fragment{
+    private static final String TAG = "CanvasFragment";
+    private CanvasView mCanvasView;
+    private Bitmap mBitmap;
+    private EditorActivity mHostingActivity;
+    private File mPhotoFile;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
+
+    }
+
+    @Override
+    public CanvasView onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView() called");
+
+        mCanvasView = new CanvasView(getContext());
+
+        mHostingActivity = (EditorActivity) getActivity();
+        mPhotoFile = new File(mHostingActivity.getPhoto().getPath());
+        //mCanvasView = (ImageView) v.findViewById(R.id.canvas);
+        if (mPhotoFile == null || !mPhotoFile.exists()) {
+            //mCanvasView.setImageDrawable(null);
+        } else {
+            mBitmap = PictureUtils.getScaledBitmap(
+                    mPhotoFile.getPath(), mHostingActivity);
+        }
+
+        mCanvasView.setImage(mBitmap);
+        
+        return mCanvasView;
+    }
+}
--- app/src/main/java/com/littlegeektoys/www/picturepurrfect/CanvasView.java
+++ app/src/main/java/com/littlegeektoys/www/picturepurrfect/CanvasView.java
@@ -1,6 +1,7 @@
 package com.littlegeektoys.www.picturepurrfect;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.view.View;
 
@@ -8,6 +9,9 @@ import android.view.View;
  * Created by Jesse DeMott on 4/22/2016.
  */
 public class CanvasView extends View {
+    private Bitmap mImage;
+    private int mCanvasWidth;
+    private int mCanvasHeight;
 
     public CanvasView(Context context)   {
         super(context);
@@ -16,6 +20,15 @@ public class CanvasView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        mCanvasWidth = canvas.getWidth();
+        mCanvasHeight = canvas.getHeight();
+        int centerX = mCanvasWidth/2;
+        int centerY = mCanvasHeight/2;
+        canvas.drawBitmap(mImage, centerX, centerY, null);
+    }
+
+    public void setImage(Bitmap image){
+        mImage = image;
     }
 
 }
